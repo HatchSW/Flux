1d1d5f93d70b0f2c771b0ffe69f347981ff1d553
Quach, Scott <quachsco000@ssd-s0073067-macbookpro.home>
2015-10-16 21:31:45 -0700

    Set-Up the picker view

--- flux/StudentDashboard_VC.swift
+++ flux/StudentDashboard_VC.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class StudentDashboard_VC: UIViewController {
+class StudentDashboard_VC: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
     
     
     var tempCourses = ["Period 1", "Period 2", "Period 3", "Period 4", "Period 5", "Period 6"]
+    
+    
+    @IBOutlet weak var coursePicker: UIPickerView!
+    
     var student: Student?
     
     @IBOutlet weak var nameLabel: UILabel!
@@ -19,14 +23,53 @@ class StudentDashboard_VC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        coursePicker.dataSource = self
+        coursePicker.delegate = self
+        
         //display student information
 //        nameLabel.text = "\(student!.firstName) \(student!.lastName)"
         
         
-        
-        
-        
+    }
+    
+   
+    
+//    http://makeapppie.com/tag/uipickerview-in-swift/
+    
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    //determines the number of rows in the picker
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tempCourses.count
+    }
+    
+    
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tempCourses[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        nameLabel.text = tempCourses[row]
+    }
+
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+        var pickerLabel = view as! UILabel!
+        if view == nil {  //if no label there yet
+            pickerLabel = UILabel()
+            //color the label's background
+            let hue = CGFloat(row)/CGFloat(tempCourses.count)
+            pickerLabel.backgroundColor = UIColor(hue: hue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+        }
+        let titleData = tempCourses[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "Georgia", size: 26.0)!,NSForegroundColorAttributeName:UIColor.blackColor()])
+        pickerLabel!.attributedText = myTitle
+        pickerLabel!.textAlignment = .Center
+        return pickerLabel
         
     }
     
-}
+    }
