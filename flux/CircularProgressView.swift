2c2f5fd889de7df6919afdee0a1863de5f1fecbe
Quach, Scott <quachsco000@ssd-s0073067-macbookpro.home>
2015-10-19 15:08:20 -0700

    Starting circular progress view

---
+++ flux/CircularProgressView.swift
@@ -0,0 +1,23 @@
+//
+//  CircularProgressView.swift
+//  flux
+//
+//  Created by Quach, Scott on 10/19/15.
+//  Copyright © 2015 Hatch. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class CircularProgressView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
